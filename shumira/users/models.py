d2d6a6985c558cab16c55cc5816b6e4e0b2e134b
Chido100 <chido_dozie@hotmail.com>
2024-03-17 13:59:15 +0000

    first commit

---
+++ shumira/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = ['email']
+
+    def __str__(self):
+        return self.username
